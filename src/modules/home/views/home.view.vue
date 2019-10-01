0e41de037b6d9c6f6e70c87d04aae33f905a4a80
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-10-01 21:11:03 +0200

    refactor(modules): update html for deisgn alignment ♻️

--- src/modules/home/views/home.view.vue
+++ src/modules/home/views/home.view.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
     <v-col cols="12" sm="12" md="12" lg="12" xl="12">
-      <v-card class="ma-4 pa-8" :flat="true" :outlined="true">
+      <v-card class="ma-4"  :flat="true" :outlined="true">
         <v-img :src="require('@/assets/logo.svg')" class="my-4" contain height="200"></v-img>
         <v-card-title class="justify-center">{{ this.config.app.title }}</v-card-title>
         <center>
