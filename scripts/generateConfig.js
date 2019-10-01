a48f51484b261cf99e92c6c0653d79ff986caaee
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-10-01 18:26:19 +0200

    fix(build): config 🐛

--- scripts/generateConfig.js
+++ scripts/generateConfig.js
@@ -5,13 +5,18 @@ const path = require('path');
 const chalk = require('chalk');
 
 // Get the current config
-const _path = path.join(process.cwd(), './src/config', 'defaults', process.env.NODE_ENV || 'development');
+if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development';
+const _path = path.join(process.cwd(), './src/config', 'defaults', process.env.NODE_ENV);
+
 let defaultConfig;
-if (fs.existsSync(`${_path}.js`)) defaultConfig = require(_path);
-else {
+if (fs.existsSync(`${_path}.js`)) {
+  defaultConfig = require(_path);
+  console.log(chalk.green(`+ Configuration based on : "${process.env.NODE_ENV}"`));
+} else {
   console.error(chalk.red(`+ Error: No configuration file found for "${process.env.NODE_ENV}" environment using development instead`));
   defaultConfig = require(path.join(process.cwd(), './src/config', 'defaults', 'development'));
 }
+
 // Get the config from  process.env.WAOS_NODE_*
 const environmentVars = _.mapKeys(
   _.pickBy(process.env, (_value, key) => key.startsWith('WAOS_VUE_')),
