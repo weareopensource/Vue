7f2a0bd58801ef6c75046f1f17aa608b8fad1535
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-10-02 07:58:25 +0200

    refactor(tasks): remove useless params ♻️

--- src/modules/tasks/views/tasks.view.vue
+++ src/modules/tasks/views/tasks.view.vue
@@ -9,7 +9,7 @@
       ></task>
       <v-row v-if="!tasks.length" align="start" justify="center">
         <v-col cols="12">
-          <v-card class="mx-6 pa-8" outlined tile width="100%">
+          <v-card class="mx-3 pa-8" outlined tile>
             <h2 class="text-center">No Tasks found :( !</h2>
           </v-card>
         </v-col>
