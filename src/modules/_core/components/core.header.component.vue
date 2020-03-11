68fa0a4026add2db44689ce1c85c06e8bf3239c4
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2020-03-11 18:59:06 +0100

    style(core): fix nav on mobile 💄

--- src/modules/_core/components/core.header.component.vue
+++ src/modules/_core/components/core.header.component.vue
@@ -1,16 +1,21 @@
 <template>
   <v-app-bar
-  :clipped-left="config.vuetify.drawer.clipped"
-  :style="{background: config.vuetify.theme.themes[theme].primary}"
-  :flat="config.vuetify.theme.flat"
-  app>
+    :clipped-left="config.vuetify.drawer.clipped"
+    :style="{background: config.vuetify.theme.themes[theme].primary}"
+    :flat="config.vuetify.theme.flat"
+    app
+  >
     <v-app-bar-nav-icon
       v-if="config.vuetify.drawer.type !== 'permanent' && config.vuetify.drawer.type !== 'mini' && (!config.vuetify.theme.navigation.displayIfLogged || isLoggedIn)"
       @click.stop="drawer = !drawer"
     ></v-app-bar-nav-icon>
     <v-app-bar-nav-icon
-      v-if="config.vuetify.drawer.type === 'mini' && (!config.vuetify.theme.navigation.displayIfLogged || isLoggedIn)"
-      @click.stop="mini = !mini"
+      v-if="config.vuetify.drawer.type === 'mini' && (!config.vuetify.theme.navigation.displayIfLogged || isLoggedIn) && !this.$vuetify.breakpoint.mdAndDown"
+      @click.stop="mini = !mini;"
+    ></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon
+      v-if="config.vuetify.drawer.type === 'mini' && (!config.vuetify.theme.navigation.displayIfLogged || isLoggedIn) && this.$vuetify.breakpoint.mdAndDown"
+      @click.stop="drawer = !drawer; mini = false;"
     ></v-app-bar-nav-icon>
     <v-toolbar-title>
       <router-link to="/">{{ config.app.title }}</router-link>
@@ -18,8 +23,8 @@
     <div class="flex-grow-1"></div>
     <v-btn v-if="!isLoggedIn" icon>
       <router-link to="/signin">
-       <v-icon>fa-user</v-icon>
-       </router-link>
+        <v-icon>fa-user</v-icon>
+      </router-link>
     </v-btn>
     <v-btn v-else @click="signout" icon>
       <v-icon>fa-arrow-right</v-icon>
@@ -40,12 +45,20 @@ export default {
   computed: {
     ...mapGetters(['theme', 'isLoggedIn']),
     drawer: {
-      get() { return this.$store.getters.drawer; },
-      set(v) { return this.$store.commit('set_drawer', v); },
+      get() {
+        return this.$store.getters.drawer;
+      },
+      set(v) {
+        return this.$store.commit('set_drawer', v);
+      },
     },
     mini: {
-      get() { return this.$store.getters.mini; },
-      set(v) { return this.$store.commit('set_mini', v); },
+      get() {
+        return this.$store.getters.mini;
+      },
+      set(v) {
+        return this.$store.commit('set_mini', v);
+      },
     },
   },
   methods: {
