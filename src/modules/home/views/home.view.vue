95a6dabf0a35202748d188e26d38263e1a0bcb1f
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2020-07-09 10:51:58 +0200

    style(home): align titles 💄

--- src/modules/home/views/home.view.vue
+++ src/modules/home/views/home.view.vue
@@ -52,7 +52,7 @@
             cols="12"
             md="6"
           >
-            <h2 class="display-2 font-weight-bold mb-3 py-8" v-text="title"></h2>
+            <h2 class="display-1 font-weight-bold mb-3 py-8  text-uppercase" v-text="title"></h2>
             <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720">
               <vue-markdown :source="text" />
             </v-responsive>
@@ -75,7 +75,7 @@
       v-if="config.home.features.data.length > 0"
     >
       <v-container class="text-center">
-        <h2 class="display-2 font-weight-bold mb-3 pb-8" v-if="config.home.features.title">{{ config.home.features.title }}</h2>
+        <h2 class="display-1 font-weight-bold mb-3 pb-8 text-uppercase" v-if="config.home.features.title">{{ config.home.features.title }}</h2>
         <v-row>
           <v-col
             v-for="({ icon, title, text }, i) in config.home.features.data"
@@ -128,9 +128,7 @@
       :style="{ background: config.vuetify.theme.themes[theme].surface }"
     >
       <v-container>
-        <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center py-8">
-          {{ config.home.blog.title }}
-        </h2>
+        <h2 class="display-1 font-weight-bold mb-3 pb-8 text-center text-uppercase" v-if="config.home.blog.title">{{ config.home.blog.title }}</h2>
         <v-row>
           <v-col v-for="({ image, text, title, link }, i) in news" :key="i" cols="12" md="4">
             <v-img :src="image" class="mb-4" height="275" max-width="100%"></v-img>
@@ -146,9 +144,7 @@
 
     <section id="contact" class="py-12">
       <v-container>
-        <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center py-8">
-          {{ config.home.contact.title }}
-        </h2>
+        <h2 class="display-1 font-weight-bold mb-3 pb-8 text-center text-uppercase" v-if="config.home.contact.title">{{ config.home.contact.title }}</h2>
         <v-theme-provider light>
           <form enctype="text/plain" method="GET" :action="config.home.contact.mail">
             <v-row>
