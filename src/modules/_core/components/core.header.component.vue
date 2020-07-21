f55d78b7536bdd450394e3f3aec448e306872c3a
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2020-07-21 10:45:00 +0200

    feat(core): manage dropdown menu for social links on phone ✨

--- src/modules/_core/components/core.header.component.vue
+++ src/modules/_core/components/core.header.component.vue
@@ -40,32 +40,57 @@
     ></v-app-bar-nav-icon>
     <v-toolbar-title>
       <router-link to="/">{{ config.app.title }}</router-link>
-      <a v-for="({ label, url }, i) in config.header.links" :key="i" :href="url" class="ml-6">{{
-        label
-      }}</a>
+      <a
+        v-for="({ label, url }, i) in config.header.links"
+        :key="i"
+        :href="url"
+        class="ml-6"
+      >{{ label }}</a>
     </v-toolbar-title>
     <div class="flex-grow-1"></div>
     <!-- custom menu -->
     <v-tooltip v-for="({ icon, label, url }, i) in config.header.socials" :key="i" bottom>
       <template v-slot:activator="{ on, attrs }">
-        <v-btn v-bind="attrs" v-on="on" min-width="50" min-height="50" x-small icon>
+        <v-btn class="hidden-sm-and-down" v-bind="attrs" v-on="on" min-width="50" min-height="50" x-small icon>
           <a :href="url">
-            <v-icon :style="{ color: config.vuetify.theme.themes[theme].onPrimary }">{{
-              icon
-            }}</v-icon>
+            <v-icon :style="{ color: config.vuetify.theme.themes[theme].onPrimary }">{{ icon }}</v-icon>
           </a>
         </v-btn>
       </template>
       <span>{{ label }}</span>
     </v-tooltip>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn class="hidden-md-and-up" v-bind="attrs" v-on="on" min-width="50" min-height="50" x-small icon>
+          <v-icon :style="{ color: config.vuetify.theme.themes[theme].onPrimary }">fa-ellipsis-h</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="({ icon, label, url }, i) in config.header.socials"
+          :key="i"
+          :href="url"
+        >
+          <v-list-item-action :style="config.vuetify.theme.navigation.selectBorder ">
+            <v-tooltip left>
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon v-bind="attrs" v-on="on">{{ icon }}</v-icon>
+              </template>
+              <span>{{ label }}</span>
+            </v-tooltip>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ label }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-menu>
     <!-- user menu -->
     <v-tooltip v-if="!isLoggedIn && config.vuetify.theme.signin" bottom>
       <template v-slot:activator="{ on, attrs }">
         <v-btn v-on="on" v-bind="attrs" min-width="50" min-height="50" x-small icon>
           <router-link to="/signin">
-            <v-icon :style="{ color: config.vuetify.theme.themes[theme].onPrimary }"
-              >fa-user</v-icon
-            >
+            <v-icon :style="{ color: config.vuetify.theme.themes[theme].onPrimary }">fa-user</v-icon>
           </router-link>
         </v-btn>
       </template>
@@ -82,9 +107,7 @@
           x-small
           icon
         >
-          <v-icon :style="{ color: config.vuetify.theme.themes[theme].onPrimary }"
-            >fa-arrow-right</v-icon
-          >
+          <v-icon :style="{ color: config.vuetify.theme.themes[theme].onPrimary }">fa-arrow-right</v-icon>
         </v-btn>
       </template>
       <span>Sign Out</span>
