51ba7cbcc4b4880a1c64debacc8469bc25ac4ee5
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2020-07-22 11:26:17 +0200

    feat(config, plugins): init matomo example for analytics ✨

---
+++ src/lib/plugins/matomo.js
@@ -0,0 +1,7 @@
+/**
+ * Module dependencies.
+ */
+import Vue from 'vue';
+import VueMatomo from 'vue-matomo';
+
+if (Vue.prototype.config.analytics.matomo && Vue.prototype.config.analytics.matomo.host) Vue.use(VueMatomo, Vue.prototype.config.analytics.matomo);
