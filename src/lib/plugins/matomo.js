fe1e9210f615e45bfca3b5cd618062095f0a1d8a
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2020-09-02 23:52:33 +0200

    fix(lib): matomo fix indent 🐛

--- src/lib/plugins/matomo.js
+++ src/lib/plugins/matomo.js
@@ -4,5 +4,6 @@
 import Vue from 'vue';
 import VueMatomo from 'vue-matomo';
 
-if (Vue.prototype.config.analytics.matomo && Vue.prototype.config.analytics.matomo.host)
+if (Vue.prototype.config.analytics.matomo && Vue.prototype.config.analytics.matomo.host) {
   Vue.use(VueMatomo, Vue.prototype.config.analytics.matomo);
+}
