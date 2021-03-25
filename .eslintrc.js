f85185e2d6ce98974b6de7f0aac59ac07f8cb2b1
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2021-03-25 15:28:12 +0100

    fix(lint): end 🐛

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)', '*.vue'],
       env: {
         jest: true,
       },
