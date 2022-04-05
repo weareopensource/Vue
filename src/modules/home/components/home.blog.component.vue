4307219286017ee5eef20eecd172e92bc322c223
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2022-04-05 09:19:57 +0200

    refactor(home): blog component to vue 3 ♻️

--- src/modules/home/components/home.blog.component.vue
+++ src/modules/home/components/home.blog.component.vue
@@ -22,9 +22,9 @@
         <v-col v-for="({ feature_image, excerpt, title, url }, i) in news" :key="i" cols="12" md="4" data-aos="fade-up">
           <!-- eslint-disable-next-line -->
           <a :href="url" target="_blank">
-            <v-img :src="feature_image" class="mb-4 rounded-xl" height="200" max-width="100%"></v-img>
+            <v-img :src="feature_image" class="mb-4 rounded-xl" height="200" max-width="100%" cover></v-img>
             <h3 class="mb-4 text--primary" v-text="title"></h3>
-            <div class="subtitle-1 text--secondary">
+            <div class="subtitle-1 text-medium-emphasis">
               <v-markdown :source="excerpt" />
             </div>
           </a>
