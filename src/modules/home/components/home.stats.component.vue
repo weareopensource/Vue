4ac2f3c6e9b04d520c230c8c85dafc30c629dfe0
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2022-04-05 09:15:44 +0200

    refactor(home): stats component to vue 3 ♻️

--- src/modules/home/components/home.stats.component.vue
+++ src/modules/home/components/home.stats.component.vue
@@ -31,16 +31,14 @@
       :height="$vuetify.display.smAndDown ? 700 : 500"
       :src="config.home.stats.background || require('@/assets/images/backgroundParalax.jpg')"
     >
-      <v-container fill-height>
-        <v-row class="mx-auto">
-          <v-col v-for="({ value, title }, i) in statistics" :key="i" cols="12" md="3">
-            <div class="text-center">
-              <div class="font-weight-black mb-4" v-text="value" data-aos="fade"></div>
-              <div class="title font-weight-regular text-uppercase" v-text="title"></div>
-            </div>
-          </v-col>
-        </v-row>
-      </v-container>
+      <v-row class="mx-auto fill-height align-center pa-12">
+        <v-col v-for="({ value, title }, i) in statistics" :key="i" cols="12" md="3">
+          <div class="text-center text-white" data-aos="fade">
+            <div class="font-weight-black text-h2 mb-4" v-text="value"></div>
+            <div class="text-uppercase" style="font-size: 125%" v-text="title"></div>
+          </div>
+        </v-col>
+      </v-row>
     </v-parallax>
   </section>
 </template>
