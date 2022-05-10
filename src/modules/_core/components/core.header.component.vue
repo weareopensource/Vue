8f7b0ff5775f6972cba9f2365630645f1f91246b
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2022-05-10 21:50:19 +0200

    refactor(core): fix header navigation bar button with vuetify 3 ♻️

--- src/modules/_core/components/core.header.component.vue
+++ src/modules/_core/components/core.header.component.vue
@@ -9,7 +9,11 @@
     <!-- Navigation button -->
     <template v-slot:prepend v-if="config.vuetify.theme.navigation.ifLogged">
       <!-- v-if="isLoggedIn" -->
-      <v-app-bar-nav-icon :style="{ color: config.vuetify.theme.themes[theme].colors.onPrimary }" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        :style="{ color: config.vuetify.theme.themes[theme].colors.onPrimary }"
+        class="ml-0"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
     </template>
     <template v-slot:prepend v-else>
       <v-app-bar-nav-icon :style="{ color: config.vuetify.theme.themes[theme].colors.onPrimary }" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
