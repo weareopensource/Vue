642fc6d0ae6ff0b030dbb7e5301f7b1461f78a60
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2023-11-03 16:02:40 +0100

    build(vue): activate compression middleware only on build for prod 🏗

--- vue.config.js
+++ vue.config.js
@@ -23,19 +23,20 @@ module.exports = defineConfig({
       if (!devServer) {
         throw new Error('webpack-dev-server is not defined');
       }
-      middlewares.unshift({
-        name: 'serve-brotli-js',
-        path: '*.js',
-        middleware: (req, res, next) => {
-          if (req.get('Accept-Encoding')?.includes('br')) {
-            req.url += '.br';
-            res.set('Content-Encoding', 'br');
-            res.set('Content-Type', 'application/javascript; charset=utf-8');
-          }
-          next();
-        },
-      });
-
+      if (process.argv.includes('build')) {
+        middlewares.unshift({
+          name: 'serve-brotli-js',
+          path: '*.js',
+          middleware: (req, res, next) => {
+            if (req.get('Accept-Encoding')?.includes('br')) {
+              req.url += '.br';
+              res.set('Content-Encoding', 'br');
+              res.set('Content-Type', 'application/javascript; charset=utf-8');
+            }
+            next();
+          },
+        });
+      }
       return middlewares;
     },
   },
