4a5c98e05f65bbe8a74eb51b0e3f2c21f8a97169
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2023-11-04 14:43:59 +0100

    Compression only for prod (#2565)
    * build(vue): enable compression only for prod build 🏗
    
    * build(vue): activate compression middleware only on build for prod 🏗

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,16 @@ const config = require('./src/config/index.cjs');
 module.exports = defineConfig({
   transpileDependencies: true,
   runtimeCompiler: true,
-  configureWebpack: {
-    plugins: [
-      new CompressionPlugin({
-        filename: '[path][base].br',
-        algorithm: 'brotliCompress',
-        test: /\.js$/,
-      }),
-    ],
+  configureWebpack: (config) => {
+    if (process.argv.includes('build')) {
+      config.plugins.push(
+        new CompressionPlugin({
+          filename: '[path][base].br',
+          algorithm: 'brotliCompress',
+          test: /\.js$/,
+        }),
+      );
+    }
   },
   devServer: {
     // proxy: 'http://localhost:3000/',
@@ -21,19 +23,20 @@ module.exports = defineConfig({
       if (!devServer) {
         throw new Error('webpack-dev-server is not defined');
       }
-      middlewares.unshift({
-        name: 'serve-brotli-js',
-        path: '*.js',
-        middleware: (req, res, next) => {
-          if (req.get('Accept-Encoding')?.includes('br')) {
-            req.url += '.br';
-            res.set('Content-Encoding', 'br');
-            res.set('Content-Type', 'application/javascript; charset=utf-8');
-          }
-          next();
-        },
-      });
-
+      if (process.argv.includes('build')) {
+        middlewares.unshift({
+          name: 'serve-brotli-js',
+          path: '*.js',
+          middleware: (req, res, next) => {
+            if (req.get('Accept-Encoding')?.includes('br')) {
+              req.url += '.br';
+              res.set('Content-Encoding', 'br');
+              res.set('Content-Type', 'application/javascript; charset=utf-8');
+            }
+            next();
+          },
+        });
+      }
       return middlewares;
     },
   },
